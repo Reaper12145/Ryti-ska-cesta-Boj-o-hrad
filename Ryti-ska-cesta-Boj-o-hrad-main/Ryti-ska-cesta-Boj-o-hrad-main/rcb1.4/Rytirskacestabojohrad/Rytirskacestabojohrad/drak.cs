b727a84c5b82ea7a02f9aa9832f8ec47d1e03abf
yxungin <120128461+yxungin@users.noreply.github.com>
2023-05-24 10:42:53 +0200

    Add files via upload

---
+++ Ryti-ska-cesta-Boj-o-hrad-main/Ryti-ska-cesta-Boj-o-hrad-main/rcb1.4/Rytirskacestabojohrad/Rytirskacestabojohrad/drak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rytirskacestabojohrad
+{
+    internal class drak : bytost
+    {
+        public string jmeno = "Drak";
+        public override int DruhaCast()
+        {
+            Console.WriteLine("{0}: Co vy zde děláte!!!!!!!!", jmeno);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0}: To je moje království!!!!!!!!!", jmeno);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0}: Puste mně!!!!!!!!!!!!", jmeno);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0}: Puste mně nebo na vás pošlu Byrua!!!!!!!!", jmeno);
+            Thread.Sleep(1000);
+            return 0;
+        }
+    }
+}
---
+++ Ryti-ska-cesta-Boj-o-hrad-main/Ryti-ska-cesta-Boj-o-hrad-main/rcb1.4/Rytirskacestabojohrad/Rytirskacestabojohrad/hospodsky.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rytirskacestabojohrad
+{
+    internal class hospodsky:bytost
+    {
+        public string jmeno = "Hospodský";
+        public override int CtvrtaCast()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}: Co si vzácní pánové nabídnou?", jmeno);
+            /*v
+            Až do noci jste chlastali a hráli karty. (-100zł)
+            Jenom jste se dobře najedli a šli jste spát. (-30zł)
+            Ještě večer jste se zlili jak prasata a užili si noc s místními prostitutkami. (-128)
+            */
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.WriteLine("1. Všichni jste si dali rundu piva a šli jste spát. (-50zł)");
+            Thread.Sleep(1000);
+            Console.WriteLine("2. Až do noci jste chlastali a hráli karty. (-100zł)");
+            Thread.Sleep(1000);
+            Console.WriteLine("3. Jenom jste se dobře najedli a šli jste spát. (-30zł)");
+            Thread.Sleep(1000);
+            Console.WriteLine("4. Ještě večer jste se zlili jak prasata a užili si noc s místními prostitutkami. (-128)");
+            Thread.Sleep(1000);
+            return 0;
+        }
+    }
+}
